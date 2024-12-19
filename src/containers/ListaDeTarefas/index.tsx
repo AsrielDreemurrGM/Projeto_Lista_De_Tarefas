69aea39908bfe31085b61481fa684412b33f574d
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-19 14:29:20 -0300

    Criado Container Para as Tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,8 @@
+import { Container } from './styles'
 const ListaDetarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-  </main>
+  </Container>
 )
 
 export default ListaDetarefas
