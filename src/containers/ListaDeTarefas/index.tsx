fdb7ef25ab71051d0cd44b55654c985c417fcb77
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-19 14:31:50 -0300

    Criado Componente Para as Tarefas
    - Removido Estilo das Listas;

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,26 @@
+import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
+
 const ListaDetarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
   </Container>
 )
 
