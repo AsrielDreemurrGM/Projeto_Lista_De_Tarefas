544e935264525472e0fe72adde22246663144533
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-19 13:24:24 -0300

    Construído e Estilizado a Barra Lateral

---
+++ src/components/FiltroCard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+import { Props } from './index'
+
+export const Card = styled.div<Props>`
+  padding: 8px;
+  color: ${(props) => (props.ativo ? '#1E90FF' : '#5e5e5e')};
+  background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
+  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#a1a1a1')};
+  border-radius: 8px;
+`
+
+export const Contador = styled.span`
+  font-weight: bold;
+  font-size: 24px;
+  display: block;
+`
+
+export const Label = styled.label`
+  font-size: 14px;
+`
