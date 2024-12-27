39f732312726f2b49acfad8f65499173143eaab7
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-12-26 22:24:14 -0300

    Configurado o React Router na Aplicação
    - Iniciado Aprendizado Sobre Single Page Applications;
    - Adicionado o Cursor Pointer ao Label do FiltroCard;

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { Provider } from 'react-redux'
-import BarraLateral from './containers/BarraLateral'
-import ListaDetarefas from './containers/ListaDeTarefas'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import EstiloGlobal, { Container } from './styles'
 
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDetarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
